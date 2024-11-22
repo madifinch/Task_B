9cd29b00e9e7d78de58f72de9989b23ac16c2067
your github name <your Github email>
2024-11-22 01:02:49 +0000

    sales day

--- sales.py
+++ sales.py
@@ -19,5 +19,7 @@ available_items:(integer) This function returns this integer which updates the a
 The function will also update the inventory_records (For restocking) for a  given current day. 
 
     '''
-    
+    if current_day % 7 == 0:
+        sold = 0 
+        
     return available_items
